ffcef89412bcf1a1e5d351fcaa4191ea398b1ab9
Carlos Gaviria <cgaviriamarquez@gmail.com>
2024-10-24 11:18:06 -0500

    Agregacion de las funciones iniciales en el dll para la union de la capa dll a dal

---
+++ DLL/Funciones del Empleados de una reservacion o varias.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace DLL
+{
+    public class Funciones_del_Empleados_de_una_reservacion_o_varias
+    {
+    }
+}
