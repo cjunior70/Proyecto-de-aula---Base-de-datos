e1e3e48a9868ca4dfbccfa22734d20941e62f2bd
Carlos Gaviria <cgaviriamarquez@gmail.com>
2024-10-28 17:29:03 -0500

    Actualizacion de la logica de servicios

--- DLL/logica de los servicios.cs
+++ DLL/logica de los servicios.cs
@@ -1,16 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DAL;
+using ENTITY;
 
 namespace DLL
 {
     public class logica_de_los_servicios
     {
+        //Conexion general par el ingreso y registro de las personas nuevas
+        Datos_login datos_de_conexion = new Datos_login();
 
+        Funcion_de_los_servicios Funciones_De_Los_Servicios=new Funcion_de_los_servicios();
 
+        //Funcion para ingresar un servicio
+        public Boolean registrar_un_servicio(Servicios datos_del_servicio)
+        {
+            Boolean confirmacion;
+
+            confirmacion = Funciones_De_Los_Servicios.Ingresar_Un_Servicio(datos_de_conexion, datos_del_servicio);
+
+            return confirmacion;
+        }
+
+        //Funcion para consultar todos los servicios disponibles
+        public DataTable funcion_para_consultar_todos_los_servicios(Servicios datos_del_servico)
+        {
+            DataTable datos;
+
+            datos = Funciones_De_Los_Servicios.Consultar_Todas_los_servicios(datos_de_conexion);
+
+            return datos;
+
+        }
+
+        //Funcion para actualizar un servicio
+        public Boolean actualizar_un_servicio( Servicios datos_del_servico) 
+        {
+            Boolean confirmacion;
+
+            confirmacion = Funciones_De_Los_Servicios.Modificar_datos_una_ubicacion(datos_de_conexion, datos_del_servico);
+
+            return confirmacion;
+
+        }
+
+        //Funcion para eliminar un servicio
+        public Boolean eliminar_un_servicio(Servicios datos_del_servicio)
+        {
+            Boolean confirmacion;
+
+            confirmacion = Funciones_De_Los_Servicios.borrar_un_servicio(datos_de_conexion, datos_del_servicio);
+
+            return confirmacion;
+
+        }
+
+        //Funcion para consultar un servicio
+        public DataTable buscar_un_servicio(Servicios datos_del_servicio)
+        {
+            DataTable datos;
+
+            datos = Funciones_De_Los_Servicios.Consultar_Un_Servicio(datos_de_conexion, datos_del_servicio);
+
+            return datos;
+
+        }
 
     }
 }
