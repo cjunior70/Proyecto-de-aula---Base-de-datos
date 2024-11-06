40da44ca37d96f33ae0a4394a0063970eeba912b
yarkit <davidyarkit@gmail.com>
2024-11-06 10:31:08 -0500

    Eso

---
+++ PRESENTACION/RegisCliente.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace PRESENTACION
+{
+    partial class RegisCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btRegistrar = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtSgApellidoU = new System.Windows.Forms.TextBox();
+            this.txtPrApellidoU = new System.Windows.Forms.TextBox();
+            this.txtSgNombreU = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPrNombreU = new System.Windows.Forms.TextBox();
+            this.txtCedulaU = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel1.Controls.Add(this.btRegistrar);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.txtSgApellidoU);
+            this.panel1.Controls.Add(this.txtPrApellidoU);
+            this.panel1.Controls.Add(this.txtSgNombreU);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtPrNombreU);
+            this.panel1.Controls.Add(this.txtCedulaU);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(958, 529);
+            this.panel1.TabIndex = 2;
+            // 
+            // btRegistrar
+            // 
+            this.btRegistrar.Font = new System.Drawing.Font("Segoe UI", 18.2F, System.Drawing.FontStyle.Bold);
+            this.btRegistrar.Location = new System.Drawing.Point(357, 419);
+            this.btRegistrar.Name = "btRegistrar";
+            this.btRegistrar.Size = new System.Drawing.Size(267, 51);
+            this.btRegistrar.TabIndex = 18;
+            this.btRegistrar.Text = "Registrarme";
+            this.btRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(650, 326);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(233, 22);
+            this.textBox5.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(650, 294);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(233, 22);
+            this.textBox4.TabIndex = 14;
+            // 
+            // txtSgApellidoU
+            // 
+            this.txtSgApellidoU.Location = new System.Drawing.Point(650, 261);
+            this.txtSgApellidoU.Name = "txtSgApellidoU";
+            this.txtSgApellidoU.Size = new System.Drawing.Size(233, 22);
+            this.txtSgApellidoU.TabIndex = 13;
+            // 
+            // txtPrApellidoU
+            // 
+            this.txtPrApellidoU.Location = new System.Drawing.Point(650, 226);
+            this.txtPrApellidoU.Name = "txtPrApellidoU";
+            this.txtPrApellidoU.Size = new System.Drawing.Size(233, 22);
+            this.txtPrApellidoU.TabIndex = 12;
+            // 
+            // txtSgNombreU
+            // 
+            this.txtSgNombreU.Location = new System.Drawing.Point(650, 192);
+            this.txtSgNombreU.Name = "txtSgNombreU";
+            this.txtSgNombreU.Size = new System.Drawing.Size(233, 22);
+            this.txtSgNombreU.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.Control;
+            this.label8.Location = new System.Drawing.Point(56, 324);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 23);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Celular";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.Control;
+            this.label7.Location = new System.Drawing.Point(56, 292);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(157, 23);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Correo electronico";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Control;
+            this.label6.Location = new System.Drawing.Point(56, 259);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(151, 23);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Segundo apellido";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(56, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(134, 23);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Primer apellido";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(56, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(149, 23);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Segundo nombre";
+            // 
+            // txtPrNombreU
+            // 
+            this.txtPrNombreU.Location = new System.Drawing.Point(650, 158);
+            this.txtPrNombreU.Name = "txtPrNombreU";
+            this.txtPrNombreU.Size = new System.Drawing.Size(233, 22);
+            this.txtPrNombreU.TabIndex = 5;
+            // 
+            // txtCedulaU
+            // 
+            this.txtCedulaU.Location = new System.Drawing.Point(650, 123);
+            this.txtCedulaU.Name = "txtCedulaU";
+            this.txtCedulaU.Size = new System.Drawing.Size(233, 22);
+            this.txtCedulaU.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 26F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(319, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(377, 60);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Datos Personales";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(56, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Primer nombre";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(56, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cedula";
+            // 
+            // RegisCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkBlue;
+            this.ClientSize = new System.Drawing.Size(982, 553);
+            this.Controls.Add(this.panel1);
+            this.Name = "RegisCliente";
+            this.Text = "RegisCliente";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btRegistrar;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtSgApellidoU;
+        private System.Windows.Forms.TextBox txtPrApellidoU;
+        private System.Windows.Forms.TextBox txtSgNombreU;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPrNombreU;
+        private System.Windows.Forms.TextBox txtCedulaU;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
