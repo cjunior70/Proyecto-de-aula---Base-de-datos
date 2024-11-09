7d7d99d67c2afbbbbca0d49720137c2311cd4f73
yarkit <davidyarkit@gmail.com>
2024-11-09 16:14:11 -0500

    r

--- PRESENTACION/PrincipalUsuario.Designer.cs
+++ PRESENTACION/PrincipalUsuario.Designer.cs
@@ -29,18 +29,43 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btRegistrar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btRegistrar);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label3);
             this.panel1.Location = new System.Drawing.Point(366, 1);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(614, 555);
             this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 26F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(40, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(496, 60);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Nombre de la empresa";
             // 
             // panel2
             // 
@@ -63,6 +88,62 @@
             this.label4.TabIndex = 8;
             this.label4.Text = "Contactos";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(63, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Dueño";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(63, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(244, 62);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Tiempo Promedio en \r\nterminar el trabajo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(63, 311);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(280, 62);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Ubicación de la empresa \r\ncomúnmente";
+            // 
+            // btRegistrar
+            // 
+            this.btRegistrar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btRegistrar.Location = new System.Drawing.Point(50, 408);
+            this.btRegistrar.Name = "btRegistrar";
+            this.btRegistrar.Size = new System.Drawing.Size(242, 40);
+            this.btRegistrar.TabIndex = 19;
+            this.btRegistrar.Text = "Actualizar localizacion";
+            this.btRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(222, 477);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 51);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Guardar Datos";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // PrincipalUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -72,6 +153,8 @@
             this.Controls.Add(this.panel1);
             this.Name = "PrincipalUsuario";
             this.Text = "PrincipalUsuario";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.ResumeLayout(false);
@@ -83,5 +166,11 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btRegistrar;
     }
 }
