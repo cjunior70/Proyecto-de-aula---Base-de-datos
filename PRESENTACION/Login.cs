1a92a227cd9d279937d1600491e2a1996f4503b7
Carlos Gaviria <cgaviriamarquez@gmail.com>
2024-11-13 07:58:13 -0500

    Agregacion de las interfaz de login

---
+++ PRESENTACION/Login.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRESENTACION
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbEntrarB_Click(object sender, EventArgs e)
+        {
+
+            if (this.txtUsuario.Text == "User" || this.txtContraseña.Text == "12345")
+            {
+                RegisCliente frm = new RegisCliente();
+                frm.Show();
+                this.Hide();
+            }
+            else
+            {
+
+                MessageBox.Show("Usuario invalido");
+
+            }
+
+        }
+    }
+}
