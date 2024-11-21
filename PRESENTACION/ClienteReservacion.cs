9ece7857835c7cff054495df437f44608775199b
yarkit <davidyarkit@gmail.com>
2024-11-21 18:02:20 -0500

    xws

--- PRESENTACION/ClienteReservacion.cs
+++ PRESENTACION/ClienteReservacion.cs
@@ -28,7 +28,7 @@ namespace PRESENTACION
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Application.Exit()
+            Application.Exit();
         }
     }
 }
